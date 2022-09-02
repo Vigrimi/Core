b3673ea3be4825defc15dd598d4a09717ef2b0e3
Vigrimi <tamotd@ya.ru>
2022-09-02 19:54:33 +0300

    #4 after reviewed

--- src/main/java/ru/vinnikov/grigory/hw1task1/Hw1Task1ClassMain.java
+++ src/main/java/ru/vinnikov/grigory/hw1task1/Hw1Task1ClassMain.java
@@ -2,6 +2,7 @@ package ru.vinnikov.grigory.hw1task1;
 
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.HashMap;
 import static java.lang.Thread.sleep;
 
 /**
@@ -13,9 +14,12 @@ public class Hw1Task1ClassMain {
     private static final int RANDOM_DIVIDER = 54321;
     private static final int[][] randomArray = new int[QTY_AND_SIZE_OF_ARRAYS][QTY_AND_SIZE_OF_ARRAYS];
     private static String buffer;
-    private static long average; // среднее значение
-    private static int MINIMUM = Integer.MAX_VALUE;
-    private static int MAXIMUM = Integer.MIN_VALUE;
+    private static final String minimumWord = "minimum";
+    private static final String maximumWord = "maximum";
+    private static final String averageWord = "average";
+    private static final int MINIMUM = Integer.MAX_VALUE;
+    private static final int MAXIMUM = Integer.MIN_VALUE;
+    private static HashMap<String, Long> minMaxAverage = new HashMap<>();
 
     public static void main(String[] args) throws InterruptedException {
         // наполнить массив рандомными числами
@@ -25,8 +29,8 @@ public class Hw1Task1ClassMain {
         findMinMaxAverage();
 
         // вывести в консоль максимальное, минимальное и среднее значение
-        System.out.println("Максимальное значение = " + MAXIMUM + ". Минимальное значение = " + MINIMUM + ". Среднее " +
-                        "значение = " + average + ".");
+        System.out.println("Максимальное значение = " + minMaxAverage.get(maximumWord) + ". Минимальное значение = "
+                + minMaxAverage.get(minimumWord) + ". Среднее значение = " + minMaxAverage.get(averageWord) + ".");
     }
 
     public static void fillinRandomArray() throws InterruptedException {
@@ -58,23 +62,26 @@ public class Hw1Task1ClassMain {
     public static void findMinMaxAverage(){
         long sum = 0;
         int qtyElements = 0;
+        int min = MINIMUM;
+        int max = MAXIMUM;
+        long average;
 
         // перебрать каждый элемент массива
         for (int i = 0; i < randomArray.length; i++) {
             for (int j = 0; j < randomArray.length; j++) {
                 sum += randomArray[i][j];
 
-                if (randomArray[i][j] < MINIMUM){
-                    MINIMUM = randomArray[i][j];
-                } else if (randomArray[i][j] > MAXIMUM){
-                    MAXIMUM = randomArray[i][j];
+                if (randomArray[i][j] < min){
+                    min = randomArray[i][j];
+                } else if (randomArray[i][j] > max){
+                    max = randomArray[i][j];
                 }
                 qtyElements++;
             }
         }
+        minMaxAverage.put(minimumWord, (long) min);
+        minMaxAverage.put(maximumWord, (long) max);
         average = sum / qtyElements;
-//        System.out.println("MINIMUM: " + MINIMUM);
-//        System.out.println("MAXIMUM: " + MAXIMUM);
-//        System.out.println("average: " + average);
+        minMaxAverage.put(averageWord, average);
     }
 }
